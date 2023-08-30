4819b36316d49b9ac1dce96e47c86f63cbbfbcc4
mdabarik <mdabarik19@gmail.com>
2023-08-30 19:44:16 +0600

    added some resource

---
+++ scripts/index.js
@@ -0,0 +1,14 @@
+
+
+const toogleDataTheme = () => {
+    const element = document.getElementById("html");
+    console.log(element);
+    const attribute = element.getAttribute('data-theme');
+    if (attribute === 'light') {
+        element.setAttribute("data-theme", "dark");
+    } else {
+        element.setAttribute("data-theme", "light");
+    }
+    
+    
+}
---
+++ scripts/typewriter.js
@@ -0,0 +1,29 @@
+const words = ["Hello, Welcome!", "Welcome to TechBarik!"];
+let i = 0;
+let j = 1;
+let currentWord = "";
+let isDeleting = false;
+
+function type() {
+  currentWord = words[i];
+  if (isDeleting) {
+    document.getElementById("typewriter").textContent = currentWord.substring(0, j-1);
+    j--;
+    if (j == 1) {
+      isDeleting = false;
+      i++;
+      if (i == words.length) {
+        i = 0;
+      }
+    }
+  } else {
+    document.getElementById("typewriter").textContent = currentWord.substring(0, j+1);
+    j++;
+    if (j == currentWord.length) {
+      isDeleting = true;
+    }
+  }
+  setTimeout(type, 200);
+}
+
+type();
